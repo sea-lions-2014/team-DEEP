362e59616b43b67c9eb7431665e1d4d5a1fecee4
Apprentice <apprentice@dbc20.local>
2014-02-20 16:41:28 -0800

    Initial setup

---
+++ entitle/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Entitle::Application.config.secret_token = 'abc97deb0417d6ab3b92adbfa30ad3be6ff278da8ae753997f4ccb0151ba2d2a6a90a3b6a290c30483eec76d73868334cd0665f7f229bfefef47b2119e08e5b8'
